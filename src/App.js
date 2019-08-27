8549860efc1e36bdb8055b3d397da75eec681b93
Joy <n.joyee123@gmail.com>
2019-08-27 14:50:38 +0800

    68. Using Radium for Meida Queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person.js';
 import './Person/Person.css';
 class App extends Component {
@@ -55,7 +56,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -75,6 +80,10 @@ class App extends Component {
          
       );
       style.backgroundColor='red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -87,6 +96,7 @@ class App extends Component {
     }
 
     return (
+      <StyleRoot>
             <div className="App">
               <h1>Hi, I'm a React App</h1>
               <p className={classes.join('  ')}>This is really working!</p>
@@ -96,11 +106,12 @@ class App extends Component {
               {persons}
                 
           </div>
+      </StyleRoot>
           );
     }
 }
 
-export default App;
+export default Radium(App);
 
 
 
