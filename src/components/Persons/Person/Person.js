64248b06dacd78332193f815320736f2aff82c75
Joy <n.joyee123@gmail.com>
2019-09-05 15:25:16 +0800

    temp

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,7 +7,7 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering')
         return (  
-                <Fragment>    
+                <Aux>    
                 <p key="i1" onClick={this.props.click}>
                 I'm a {this.props.name} and I am {this.props.age}  years old!
                 </p>
@@ -16,7 +16,7 @@ class Person extends Component {
                 type="text" 
                 onChange={this.props.changed} 
                 value={this.props.name} />
-                </Fragment> 
+                </Aux> 
             
          );
     }
